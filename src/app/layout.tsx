6cdc62422a39490c9e6117788197ed3e8e99e74d
luarodriguess <lua.rodrigues@lydia.com.br>
2025-07-27 18:53:41 -0300

    fix: remove global loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import GlobalRouteLoader from "./packages/components/loading/global-loading";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-         <GlobalRouteLoader />
         {children}
       </body>
     </html>
